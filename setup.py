d77a47f974c068bcc43641ccb1fb78907e808061
Fishified <duguay.jason@gmail.com>
2019-05-16 08:08:49 -0400

    Add package requirements

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyFVM',
+      version='0.1',
+      description='Python 3.6 translation of uFVM',
+      url='http://github.com/storborg/funniest',
+      author='Flying circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['pyFVM'],
+      zip_safe=False)
