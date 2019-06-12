209bde62d5a3e452132610fb94b34c95704c5739
Fishified <duguay.jason@gmail.com>
2019-06-12 08:00:48 -0400

    add cfdReadUniformVolVectorFieldValue.py

---
+++ pyFVM/utilities/IO/File/cfdReadUniformVolVectorFieldValue.py
@@ -0,0 +1,31 @@
+
+def cfdReadUniformVolVectorFieldValue(volVectorFieldEntry):
+
+    """Returns [u,v,w] type list from a 'value uniform (u v w)' dictionary entry. 
+    
+    Basically strips off '(' and ')' and returns a python list object, e.g. 
+    [0, 1.2, 5]
+    
+    Attributes:
+        
+       volVectorFieldEntry (list): list containing ['uniform', '(u', 'v','w)']
+       
+    Example usage:
+        
+        Region = cfdReadUniformVolVectorFieldValue(volVectorFieldEntry)
+        
+    """    
+    
+    vector=[]
+    
+    for item in volVectorFieldEntry:
+        
+        if item == 'uniform':
+            continue
+    
+        item=item.replace("(","")
+        item=item.replace(")","")
+        
+        vector.append(float(item))
+        
+    return vector
