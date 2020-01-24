c6ac561d94a8354f0653a50f47bf44b8af928a53
Fishified <duguay.jason@gmail.com>
2020-01-24 06:41:22 -0500

    remove pychache folder

--- example/devScript.py
+++ example/devScript.py
@@ -5,9 +5,6 @@ import pyFVM.Region as Region
 import numpy as np
 
 
-
 region=Region.Region(os.getcwd())
 region_vars=vars(region)
-
-
 
--- pyFVM/Region.py
+++ pyFVM/Region.py
@@ -131,7 +131,6 @@ class Region():
 
         ###########################
 
-
     def cfdGeometricLengthScale(self):
     
         """
